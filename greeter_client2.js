3cbbffb7a4d64bf29e3368e7ad6e1644f209c641
gtrujilloca <gtrujilloca@gmail.com>
2020-09-26 22:52:03 -0500

    update .proto

--- greeter_client2.js
+++ greeter_client2.js
@@ -1,14 +1,16 @@
 const grpc = require("grpc");
-const protoLoader = require("@grpc/proto-loader")
+const protoLoader = require("@grpc/proto-loader");
 const packageDef = protoLoader.loadSync("helloworld.proto", {});
 const grpcObject = grpc.loadPackageDefinition(packageDef);
 const helloworld = grpcObject.helloworld;
 
-const num = process.argv[2];
-const option = process.argv[3];
+const num = process.argv[3];
+const option = process.argv[2];
 
-const client = new helloworld.Greeter("0.0.0.0:50051", 
-grpc.credentials.createInsecure())
+const client = new helloworld.Greeter(
+  "0.0.0.0:50051",
+  grpc.credentials.createInsecure()
+);
 
 /* 
 client.SayHello({
@@ -19,17 +21,20 @@ client.SayHello({
 
 }) */
 
-if(option==1){
-    client.getValue(null,(err, response) => {
-     console.log("Recieved from getValue " + JSON.stringify(response))
-    })
+switch (option) {
+  case '1':
+    client.getValue(null, (err, response) => {
+        console.log("Recieved from getValue " + JSON.stringify(response));
+      });
+    break;
+  case '2':
+    client.setValue({ value: parseInt(num) }, (err, response) => {
+        console.log("Recieved from server " + JSON.stringify(response));
+      });
+    break;
+  case '3':
+    client.getPathologies(null, (err, response) => {
+        console.log("Recieved from getPatologies " + JSON.stringify(response));
+      });
+    break;
 }
-
-if(option==2){
-    client.setValue({"value":num},(err, response) => {
-        console.log("Recieved from server " + JSON.stringify(response))
-    })
-}
-
-
-
